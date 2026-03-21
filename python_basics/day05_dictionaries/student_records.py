6d7a690e70b2ceb30fa9faceb15a7e2fc488c45f
mohit <mohitvermakanha9415@gmail.com>
2026-03-21 20:49:14 +0530

    Day 5: update student_record fn ( add avg_marks fn)

--- python_basics/day05_dictionaries/student_records.py
+++ python_basics/day05_dictionaries/student_records.py
@@ -12,8 +12,11 @@ def add_student(name, marks):
 def get_topper():
     return max(students, key=students.get)
 
+def avg_marks():
+    return sum(students.values()) / len(students)
 
 add_student("David", 95)
 
 print("All Students:", students)
 print("Topper:", get_topper())
+print("Average marks:", avg_marks())
