249f9338e7e016f7e5b03b4e1781a99fdb2bbad3
mohit <mohitvermakanha9415@gmail.com>
2026-03-22 20:50:48 +0530

    Day 6: Functions and recursion (factorial, GCD, modular calculator)

---
+++ python_basics/day06_functions_recursion/factorial.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    
+    # Recursive case
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+print("Factorial:", factorial(num))
---
+++ python_basics/day06_functions_recursion/modular_calculator.py
@@ -0,0 +1,31 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+print("Choose operation: +, -, *, /")
+op = input("Enter operator: ")
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", add(a, b))
+elif op == "-":
+    print("Result:", subtract(a, b))
+elif op == "*":
+    print("Result:", multiply(a, b))
+elif op == "/":
+    print("Result:", divide(a, b))
+else:
+    print("Invalid operator")
